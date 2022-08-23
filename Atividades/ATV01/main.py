0b9d4c9189d71d057d02cd832c4c2b1e9c2f56e8
Luis Ricardo <luis.ricardo@tech4h.com.br>
2022-08-23 09:08:38 -0300

    refactor and comments

--- Atividades/ATV01/main.py
+++ Atividades/ATV01/main.py
@@ -3,13 +3,24 @@ import metodos as mt
 import numpy as np
 
 if __name__ == '__main__':
-    for arquivo in sys.argv[1:]:
-        matriz = mt.criar_matriz(arquivo)
-        print(f'{"-"*3} {arquivo} {"-"*3}')
+    # A loop that iterates over the files passed as arguments to the program.
+    for file in sys.argv[1:]:
+        
+        # Creating a matrix from the file.
+        matriz = mt.create_grid(file)
+        
+        # Printing a line of dashes before and after the file name.
+        print(f'{"-"*3} {file} {"-"*3}')
+        
+        # Printing the matrix.
         print(matriz)
 
-        resultado = f'{arquivo} {str(matriz.shape)}'
+        # Creating a string with the name of the file and the shape of the matrix.
+        result = f'{file} {str(matriz.shape)}\n'
 
-        print(f'Resultado: {resultado}\n')
+        # Printing the result of the file and the shape of the matrix.
+        print(f'Resultado: {result}')
 
-        mt.salvar_arquivo('resultado.txt', resultado)
+        # Saving the result of the file and the shape of the matrix in a file
+        # called resultado.txt.
+        mt.save_file('resultado.txt', result)
--- Atividades/ATV01/metodos.py
+++ Atividades/ATV01/metodos.py
@@ -1,15 +1,28 @@
 import numpy as np
 
-def salvar_arquivo(nome_arquivo, conteudo):
-    caminho = f'./{nome_arquivo}'
-    with open(caminho, 'a') as arquivo:
-        arquivo.write(conteudo)
+"""
+    > The function takes in two arguments, a file name and content, and writes the
+    content to the file
+    
+    :param file_name: The name of the file you want to save
+    :param content: the content to be written to the file
+"""
+def save_file(file_name, content):
+    file_path = f'./{file_name}'
+    with open(file_path, 'a') as file:
+        file.write(content)
+    file.close()
 
+"""
+    > It reads the file and returns a numpy array
+    
+    :param file_name: the name of the file that contains the grid
+    :return: A matrix with the values of the file.
+"""
+def create_grid(file_name):
+    file_path = f'./dados/{file_name}.txt'
 
-def criar_matriz(nome_arquivo):
-    caminho = f'./dados/{nome_arquivo}.txt'
-
-    with open(caminho, 'r') as arquivo:
-        data = np.loadtxt(arquivo, dtype=int, delimiter=' ')
+    with open(file_path, 'r') as file:
+        data = np.loadtxt(file, dtype=int, delimiter=' ')
 
     return data
